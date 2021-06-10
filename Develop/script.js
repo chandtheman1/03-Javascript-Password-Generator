2d586b48d72ff127cb09a990b91a2a3431b3c54c
Chandler Liang <chandler.liang123@gmail.com>
2021-06-10 14:37:03 +1000

    first changes, defining varaibles

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,27 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+do{
+  var lengthEl = parseInt(window.prompt("Please enter a number from 8 to 128", ""), 10);
+}while(isNaN(lengthEl) || lengthEl > 128 || lengthEl < 8);
+
+var lowerCaseEl = confirm("Do you want lowercase?");
+var upperCaseEl = confirm("Do you want uppercase?");
+var numberEl = confirm("Do you want numbers?");
+var symbolEl = confirm("Do you want special symbols?");
+
+
+
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var length = lengthEl.value;
+  var hasLower = lowerCaseEl.checked;
+  var hasUpper = upperCaseEl.checked;
+  var hasNumber = numberEl.checked;
+  var hasSymbol = symbolEl.checked;
+
+  var password = generatePassword(length, hasLower, hasUpper, hasNumber, hasSymbol);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -11,3 +29,42 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// generates randowm lower case letters
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+// generates random upper case letters
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+// generates random number
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+// generates random symbol
+function getRandomSymbol () {
+  var symbols = "!@#$%^&*()[]{}=<>/,.";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+function generatePassword(length, hasLower, hasUpper, hasNumber, hasSymbol) {
+
+
+  var generatedPassword = "";
+  var typesCount = hasLower + hasUpper + hasNumber + hasSymbol;
+
+  var typesArr = [{hasLower}, {hasUpper}, {hasNumber}, {hasSymbol}].filter
+  (
+    item => Object.values(item)[0]
+  );
+  
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach(type => {
+      
+    });
+  }
+}
+
